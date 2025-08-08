f436e5aadd46ba25950ebc64a62540c3b9f4e013
satenderDagar <satender.dagar10@gmail.com>
2025-08-08 06:29:29 +0530

    Initial Commit

---
+++ ANZ/ANZApp.swift
@@ -0,0 +1,17 @@
+//
+//  ANZApp.swift
+//  ANZ
+//
+//  Created by Satender Dagar on 08/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ANZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
